6fa559387a50e68629b1fa7f344bf41797ae696b
Akaanksh Raj <akaankshraj@gmail.com>
2018-04-21 20:40:31 +0000

    fix changeLatency()

--- api/v2/manager.php
+++ api/v2/manager.php
@@ -219,11 +219,11 @@ function addToDB($data) {
         }
         
         $apdUpdateUS = (( $apdDataFromDBUS[0] + ($apdDataFromDBUS[1] / 2) ) / ($apdDataFromDBUS[0] + $apdDataFromDBUS[1] + $apdDataFromDBUS[2] + 1));
-        $apdUpdateIE = (( $apdDataFromDBIE[0] + ($apdDataFromDBIE[1] / 2) ) / ($apdDataFromDBUS[0] + $apdDataFromDBUS[1] + $apdDataFromDBUS[2] + 1));
+        $apdUpdateIE = (( $apdDataFromDBIE[0] + ($apdDataFromDBIE[1] / 2) ) / ($apdDataFromDBIE[0] + $apdDataFromDBIE[1] + $apdDataFromDBIE[2] + 1));
         $apdWKUpdateUS = (( $apdWKDataFromDBUS[0] + ($apdWKDataFromDBUS[1] / 2) ) / ($apdWKDataFromDBUS[0] + $apdWKDataFromDBUS[1] + $apdWKDataFromDBUS[2] + 1));
-        $apdWKUpdateIE = (( $apdWKDataFromDBIE[0] + ($apdWKDataFromDBIE[1] / 2) ) / ($apdWKDataFromDBUS[0] + $apdWKDataFromDBUS[1] + $apdWKDataFromDBUS[2] + 1));
+        $apdWKUpdateIE = (( $apdWKDataFromDBIE[0] + ($apdWKDataFromDBIE[1] / 2) ) / ($apdWKDataFromDBIE[0] + $apdWKDataFromDBIE[1] + $apdWKDataFromDBIE[2] + 1));
         $apdMNUpdateUS = (( $apdMNDataFromDBUS[0] + ($apdMNDataFromDBUS[1] / 2) ) / ($apdMNDataFromDBUS[0] + $apdMNDataFromDBUS[1] + $apdMNDataFromDBUS[2] + 1));
-        $apdMNUpdateIE = (( $apdMNDataFromDBIE[0] + ($apdMNDataFromDBIE[1] / 2) ) / ($apdMNDataFromDBUS[0] + $apdMNDataFromDBUS[1] + $apdMNDataFromDBUS[2] + 1));
+        $apdMNUpdateIE = (( $apdMNDataFromDBIE[0] + ($apdMNDataFromDBIE[1] / 2) ) / ($apdMNDataFromDBIE[0] + $apdMNDataFromDBIE[1] + $apdMNDataFromDBIE[2] + 1));
         
         $checks = $checks + 1; $checksWK = $checksWK + 1; $checksMN = $checksMN + 1;
         $sslUS = $data["us"]["ssl"]; $sslIE = $data["ie"]["ssl"];
